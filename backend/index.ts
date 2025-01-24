dba0fae90e01745dc7ecab442ff0747c46120fb8
brunomaffei <bruno.maffei@hotmail.com>
2025-01-24 12:50:58 -0300

    backend

--- backend/index.ts
+++ backend/index.ts
@@ -27,11 +27,9 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "https://serviceflow-frontend-brunomaffei.vercel.app",
-      "https://serviceflow-r5m9.vercel.app",
       "http://localhost:5173",
       "http://localhost:3001",
-      "https://serviceflow-frontend.vercel.app", // adicione essa origem sem a barra no final
+      "https://serviceflow-9coy.vercel.app/",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization", "admin-id"],
